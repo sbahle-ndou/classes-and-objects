9b82a0273948b74d5bb307d82d0104eb50c19713
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 12:25:09 +0200

    Add project files.

---
+++ classes and objects/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ classes and objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+            book1.title = "Harr Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the rings";
+            book2.author = "Tolkein";
+            book2.pages = 900;
+
+            Console.WriteLine(book1.pages);
+            Console.WriteLine(book2.title);
+            Console.ReadLine();
+        }
+    }
+}
